e818736124246a3316d1c9cd42dcb795fd084614
Thomas Tran <timtom9801@gmail.com>
2022-04-26 00:27:13 -0700

    initial pushes

---
+++ main.h
@@ -0,0 +1,10 @@
+class Board{
+    public:
+        Board(); //default constructor
+
+        //funcs:
+        void performSeach(); //performs the search algorithm based on which one is choosen (parameters: )
+        int uniformSearch(); //returns ... 
+        int misplacedTileHeur(); //returns ...
+        int euclideanDistanceHeur(); //returns...
+};
